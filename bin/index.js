8feb9ad23de65d1f3dc80e3771ffa209a1b6bb23
David Alves <dev.david@aticonsultoria.com>
2016-09-06 14:57:31 -0300

    Initial commit

---
+++ bin/index.js
@@ -0,0 +1,83 @@
+#! /usr/bin/env node
+
+'use strict';
+
+const https = require('https');
+const querystring = require('querystring');
+const meow = require('meow');
+
+const cli = meow(`
+    Modo de uso:
+      $ stat <package-name>
+
+    Exemplo:
+      $ tempo clima-app
+
+`, {});
+
+let busca = cli.input.join(' ');
+
+if (busca.length === 0) {
+	console.log('Ops, você precisa informar um pacote!');
+	return;
+}
+
+let cidade = querystring.stringify({ address: busca });
+
+https
+	.get(`https://maps.googleapis.com/maps/api/geocode/json?${cidade}&components=country:BR`, 
+		(response) => {
+		let body = '';
+		response.on('data', (data) => {
+			body += data;
+		});
+		response.on('error', (e) => {
+			console.log('\nOps, cidade não encontrada! :(\n');
+		});
+		response.on('end', () => {
+
+			let results = JSON.parse(body).results[0];
+
+			if (results.types.indexOf('locality') === -1) {
+				console.log('\nOps, você digitou uma cidade inválida!\n');
+				return;
+			}
+
+			let nome_cidade = results.formatted_address.split(', ')[0];
+
+			let estado = results.address_components.reverse()[1].short_name;
+
+			let location = results.geometry.location;
+      		
+      		forecast
+				.latitude(location.lat)
+				.longitude(location.lng)
+				.get()
+				.then((res) => {
+					res = JSON.parse(res).currently;            
+			    	let temperatura = (res.temperature - 32) / 1.8;
+			    	let icone = res.icon.replace(/\W/g, '');
+			    	console.log(`\n${nome_cidade.toUpperCase()} - ${estado}: ${eval('mensagem.' + icone)}` +
+			    	 	` A temperatura no momento está em torno de ${Math.round(temperatura)}°c.\n`);
+			    })
+			    .catch((err) => {
+			        console.log('\nOps, algo de errado aconteceu. :( \n\n' + 
+			        	`Error:  ${err.split('\n')[1]}` +
+			        	'\n\nPor favor, informe o erro acima em: https://github.com/davidalves1/clima-app/issues' + 
+			        	'\nObrigado! :)\n');
+				});
+		});
+	});
+
+const mensagem = {
+	clearday: 'Dia com céu claro.', 
+	clearnight: 'Noite com céu estrelado.',	 
+	rain: 'Prepare o guarda chuva, pois parece que vai chover.', 
+	snow: 'Tire as cobertas do guarda-roupas, pois parece que o frio vem com tudo.', 
+	sleet: 'Prepare seu chá, pois parece que vem chuva e frio.', 
+	wind: 'Macacos me mordam! Que vento é esse hein?!', 
+	fog: 'Hoje a neblina parece ter vindo com tudo.', 
+	cloudy: 'Tempo encoberto.', 
+	partlycloudyday: 'Dia parcialmente nublado.',
+	partlycloudynight: 'Noite parcialmente nublada.'
+};
---
+++ readme.md
@@ -0,0 +1,24 @@
+# stat-pack-npm
+
+Verifique as estatísticas dos pacotes do NPM
+
+## Instalação
+
+```shell
+$ npm install -g stat-pack-npm
+```
+
+## Modo de usar
+
+<!-- ```shell
+$ tempo <cidade>
+$ tempo São Paulo
+``` -->
+
+### Contribuições
+
+Crie uma [issue](https://github.com/davidalves1/stat-pack-npm/issues/new) ou envie um **pull request** e nos ajude a melhorar o projeto, toda contribuição é muito bem vinda!
+
+### Licença
+
+ISC © David Alves de Souza
