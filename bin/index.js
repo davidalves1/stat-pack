39259039e1ae042ca0d563e6f21d8a10eee57d31
davidalves1 <alves.david@outlook.com>
2016-09-07 00:03:19 -0300

    Alterações para realizar a consulta básica

--- bin/index.js
+++ bin/index.js
@@ -8,76 +8,50 @@ const meow = require('meow');
 
 const cli = meow(`
     Modo de uso:
-      $ stat <package-name>
+      $ stat <pacote-name> 
+
+    Options
+      --period, -p Define um período: last-day, last-week, last-month
 
     Exemplo:
-      $ tempo clima-app
+      $ stat clima-app
+
+
 
 `, {});
 
-let busca = cli.input.join(' ');
+let pacote = cli.input.join(' ');
 
-if (busca.length === 0) {
+if (pacote.length === 0) {
 	console.log('Ops, você precisa informar um pacote!');
 	return;
 }
 
-let cidade = querystring.stringify({ address: busca });
+let period = 'last-month';
 
 https
-	.get(`https://maps.googleapis.com/maps/api/geocode/json?${cidade}&components=country:BR`, 
+	.get(`https://api.npmjs.org/downloads/point/${period}/${pacote}`, 
 		(response) => {
 		let body = '';
 		response.on('data', (data) => {
 			body += data;
 		});
 		response.on('error', (e) => {
-			console.log('\nOps, cidade não encontrada! :(\n');
+			console.log('\nOps, pacote não encontrado! :(\n');
 		});
 		response.on('end', () => {
 
-			let results = JSON.parse(body).results[0];
+			let results = JSON.parse(body);
 
-			if (results.types.indexOf('locality') === -1) {
-				console.log('\nOps, você digitou uma cidade inválida!\n');
-				return;
+			if (results.error !== undefined)
+				console.log(`Ops, parece que o pacote ${pacote} não foi encontrado`);
+			else {
+				console.log(`O pacote ${pacote} teve ${results.downloads} downloads entre os dias ${formatDate(results.start)} e ${formatDate(results.end)}`);
 			}
 
-			let nome_cidade = results.formatted_address.split(', ')[0];
-
-			let estado = results.address_components.reverse()[1].short_name;
-
-			let location = results.geometry.location;
-      		
-      		forecast
-				.latitude(location.lat)
-				.longitude(location.lng)
-				.get()
-				.then((res) => {
-					res = JSON.parse(res).currently;            
-			    	let temperatura = (res.temperature - 32) / 1.8;
-			    	let icone = res.icon.replace(/\W/g, '');
-			    	console.log(`\n${nome_cidade.toUpperCase()} - ${estado}: ${eval('mensagem.' + icone)}` +
-			    	 	` A temperatura no momento está em torno de ${Math.round(temperatura)}°c.\n`);
-			    })
-			    .catch((err) => {
-			        console.log('\nOps, algo de errado aconteceu. :( \n\n' + 
-			        	`Error:  ${err.split('\n')[1]}` +
-			        	'\n\nPor favor, informe o erro acima em: https://github.com/davidalves1/clima-app/issues' + 
-			        	'\nObrigado! :)\n');
-				});
-		});
+		})
 	});
 
-const mensagem = {
-	clearday: 'Dia com céu claro.', 
-	clearnight: 'Noite com céu estrelado.',	 
-	rain: 'Prepare o guarda chuva, pois parece que vai chover.', 
-	snow: 'Tire as cobertas do guarda-roupas, pois parece que o frio vem com tudo.', 
-	sleet: 'Prepare seu chá, pois parece que vem chuva e frio.', 
-	wind: 'Macacos me mordam! Que vento é esse hein?!', 
-	fog: 'Hoje a neblina parece ter vindo com tudo.', 
-	cloudy: 'Tempo encoberto.', 
-	partlycloudyday: 'Dia parcialmente nublado.',
-	partlycloudynight: 'Noite parcialmente nublada.'
-};
+function formatDate(word) {
+	return word.split('-').reverse().join('-')
+}
